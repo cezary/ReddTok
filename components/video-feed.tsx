033e4c21fc8c19648cf76e98f8ba24360ba30e2e
cezary <cezary@gmail.com>
2025-02-05 09:38:30 -0600

    add error message

--- components/video-feed.tsx
+++ components/video-feed.tsx
@@ -2,7 +2,7 @@
 
 import VideoList  from '@/components/video-list';
 import { useGetVideos } from '@/lib/api';
-import { MdiTelevisionOff, MingcuteLoadingFill } from './icons';
+import { MdiAlert, MdiTelevisionOff, MingcuteLoadingFill } from './icons';
 import { useSearchParams } from 'next/navigation';
 import { Sort, SORTS } from '@/lib/types';
 
@@ -18,7 +18,7 @@ export default function VideoFeed({
   const searchParams = useSearchParams();
   const sortParam = searchParams.get('sort');
   const sort: Sort = ((sortParam && (SORTS as readonly string[]).includes(sortParam)) ? sortParam : 'hot') as Sort;
-  const { data: videos, isLoading } = useGetVideos({ postId, sort, subreddit, username });
+  const { data: videos, error, isLoading } = useGetVideos({ postId, sort, subreddit, username });
 
   return (
     isLoading ?
@@ -26,6 +26,15 @@ export default function VideoFeed({
         <div className='flex gap-2 items-center text-xl'>
           <MingcuteLoadingFill className='animate-spin' /> Loading
         </div>
+      </div> :
+    error ?
+      <div className="grid place-items-center h-dvh w-fill">
+      <div className='flex flex-col gap-4 items-center'>
+        <div className='flex gap-2 items-center text-xl'>
+          <MdiAlert /> There was an error fetching videos :(
+        </div>
+        <div>Message: {error.message}</div>
+      </div>
       </div> :
     videos?.length === 0 ?
       <div className="grid place-items-center h-dvh w-fill">
