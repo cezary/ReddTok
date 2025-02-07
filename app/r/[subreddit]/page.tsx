3af283c23bac99a5c4ffc5f3b5e1a367f6870559
cezary <cezary@gmail.com>
2025-02-07 12:59:15 -0600

    redirect to main page

--- app/r/[subreddit]/page.tsx
+++ app/r/[subreddit]/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 
 import VideoFeed from '@/components/video-feed';
 import App from '@/components/app';
+import { redirect } from 'next/navigation';
 
 interface Props {
   params: Promise<{
@@ -24,6 +25,7 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
 export default async function Home(props: Props) {
   const params = await props.params;
 
+  return redirect('/');
   return (
     <App>
       <VideoFeed {...params} />
